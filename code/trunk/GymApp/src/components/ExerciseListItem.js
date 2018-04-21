fcb2ba60e8d558acfeaad1b7ae4df1953eabf0f6
ms853 <ms853@0502792f-93a2-4a5a-9ab4-ce38d4247417>
2018-04-21 15:31:48 +0000

    ExerciseListItem is made to display individual records of the data retrieved from the database.

---
+++ code/trunk/GymApp/src/components/ExerciseListItem.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import {Text, View, TouchableWithoutFeedback} from 'react-native';
+import { CardSection } from '../components/reusable'
+
+class ExerciseListItem extends Component{
+    
+    render() {
+        const { exercise_name } = this.props.exercise;
+        
+        return (
+            <TouchableWithoutFeedback /**onPress={this.onRowPress.bind(this)}**/>
+                <View>
+                    <CardSection>
+                        <Text style={styles.textStyle}>{exercise_name}</Text>
+                    </CardSection>
+                </View>
+            </TouchableWithoutFeedback>        
+        );
+    }
+    
+}
+
+const styles = {
+    textStyle: {
+        fontSize: 20,
+        paddingLeft: 18
+    }
+};
+
+export default ExerciseListItem;
