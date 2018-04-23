2964641fc60983db3c6113621e8220e2a72d2f46
ms853 <ms853@0502792f-93a2-4a5a-9ab4-ce38d4247417>
2018-04-23 00:38:00 +0000

    Committing new component for displaying videos which is the workout tutorial component.

---
+++ code/trunk/GymApp/src/components/main/WorkoutTutorials.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, Text, ScrollView } from 'react-native';
+
+class WorkoutTutorials extends Component{
+    render() {
+        return (
+            <ScrollView>
+                <View>
+                    <Text> Workout Tutorials </Text>
+                </View>
+            </ScrollView>
+        );
+    }
+}
+
+export default WorkoutTutorials;
