e9c63339b85aac6ed4b8c699d971a73fcf8ac9d6
ms853 <ms853@0502792f-93a2-4a5a-9ab4-ce38d4247417>
2018-04-25 14:36:22 +0000

    I added the register reducer to my combined reducers function.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@
 import { combineReducers } from "redux";
 import AuthReducer from "./AuthReducer";
 import AddWorkoutReducer from "./AddWorkoutReducer";
+import RegisterReducer from "./RegisterReducer";
 import ExerciseReducer from "./ExerciseReducer";
 
 export default combineReducers({
     auth: AuthReducer, //reducer property for authentication
+    register: RegisterReducer, //reducer property for registering a new user 
     workout: AddWorkoutReducer, //reducer property for adding exercises
     exercises: ExerciseReducer //The exercises state will come from exercise reducer.
 }); 
