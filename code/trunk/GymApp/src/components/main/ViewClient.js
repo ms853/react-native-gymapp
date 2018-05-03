aa6782f62a87b4b56ed96e20c8e6316a06278c15
ms853 <ms853@0502792f-93a2-4a5a-9ab4-ce38d4247417>
2018-05-03 21:02:42 +0000

    Adding changes to my project - uploaded new component ViewClient.js

---
+++ code/trunk/GymApp/src/components/main/ViewClient.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { Header } from "react-native-elements";
+import { Card, CardSection, Input, Button } from "./reusable";
+
+class CreateWorkoutPlanner extends Component{
+    render(){
+        return(
+
+            <View>
+                <Text>View Client Details</Text>
+            </View>
+        );
+    }
+}
+
+export default CreateWorkoutPlanner;
