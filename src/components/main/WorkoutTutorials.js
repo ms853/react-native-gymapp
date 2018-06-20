503b6461cf44a3d54f2f990127817b1c7dd577aa
Michael Sakyiama <ms853@student.le.ac.uk>
2018-06-21 00:28:03 +0100

    changes made to this component, need to modify the videos

--- src/components/main/WorkoutTutorials.js
+++ src/components/main/WorkoutTutorials.js
@@ -4,6 +4,23 @@ import Video from "react-native-video";
 
 class WorkoutTutorials extends Component{
     render() {
+
+        // Within your render function, assuming you have a file called
+        // "background.mp4" in your project. You can include multiple videos
+        // on a single screen if you like.
+
+        
+
+        // // Later to trigger fullscreen
+        // this.player.presentFullscreenPlayer()
+
+        // // Disable fullscreen
+        // this.player.dismissFullscreenPlayer()
+
+        // // To set video position in seconds (seek)
+        // this.player.seek(0)
+
+       
         return (
             <ImageBackground 
                 style={styles.imageBackground}
@@ -13,8 +30,23 @@ class WorkoutTutorials extends Component{
                     <View>
                         <Text> Workout Tutorials </Text>
                     </View>
-                    <Video />
-                </ScrollView>
+                    <Video //source={{uri: "Video1"}}
+                        source={require('../../assets/videos/Video1.mp4')}   // Can be a URL or a local file.
+                        ref={(ref) => {
+                            this.player = ref
+                        }}                                      // Store reference
+                        onBuffer={this.onBuffer}                // Callback when remote video is buffering
+                        onEnd={this.onEnd}                      // Callback when playback finishes
+                        onError={this.videoError}               // Callback when video cannot be loaded
+                        onFullscreenPlayerWillPresent={this.fullScreenPlayerWillPresent} // Callback before fullscreen starts
+                        onFullscreenPlayerDidPresent={this.fullScreenPlayerDidPresent}   // Callback after fullscreen started
+                        onFullscreenPlayerWillDismiss={this.fullScreenPlayerWillDismiss} // Callback before fullscreen stops
+                        onFullscreenPlayerDidDismiss={this.fullScreenPlayerDidDismiss}  // Callback after fullscreen stopped
+                        onProgress={this.setTime}               // Callback every ~250ms with currentTime
+                        onTimedMetadata={this.onTimedMetadata}  // Callback when the stream receive some metadata
+                        style={styles.backgroundVideo} />                
+                        
+                        </ScrollView>
             </ImageBackground>    
         );
     }
@@ -25,8 +57,15 @@ const styles = {
         width: '100%',
         height: '100%',
         alignSelf: 'stretch' 
-    }
+    },
 
+    backgroundVideo: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0
+    }
 };
 
 export default WorkoutTutorials;
